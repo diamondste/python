f36d84feaab03c9d88907b7d415ab1c05553b6e0
Diamond Stephens <diamond.stephens2@gmail.com>
2021-12-04 20:14:45 -0500

    commit -m "added user.py"

---
+++ flask_mysql/crud/CR/user.py
@@ -0,0 +1,22 @@
+from mysqlconnection import connectToMySQL
+
+class User: 
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+
+        results = connectToMySQL('users_schema').query_db(query)
+
+        users = []
+
+        for user in results: 
+            friends.append( cls(user) )
+        return users

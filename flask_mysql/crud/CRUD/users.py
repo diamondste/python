685efae7946cdaa3880bebaca3ef5aac5e1eb383
Diamond Stephens <diamond.stephens2@gmail.com>
2021-12-05 19:44:04 -0500

    commit -m "update edit"

--- flask_mysql/crud/CRUD/users.py
+++ flask_mysql/crud/CRUD/users.py
@@ -38,7 +38,7 @@ class User:
     
     @classmethod
     def update(cls,data):
-        query = "UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s, updated_at= NOW() WHERE id = %(id)s;"
+        query = "UPDATE users SET first_name = %(first_name)s , last_name = %(last_name)s , email = %(email)s , updated_at= NOW() WHERE id = %(id)s;"
         return connectToMySQL('users_schema').query_db(query, data)
     
     @classmethod

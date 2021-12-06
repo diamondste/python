8ba9740d269f1c52497c336f37c58faa4cdf8ac9
Diamond Stephens <diamond.stephens2@gmail.com>
2021-12-05 19:30:10 -0500

    commit -m "update user"

---
+++ flask_mysql/crud/CRUD/server.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, redirect
+
+from users import User
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect('/users')
+
+@app.route('/users')
+def users(): 
+    return render_template("users.html", users = User.get_all())
+
+
+@app.route('/user/new')
+def new_user():
+    return render_template("new_user.html")
+
+@app.route('/user/create', methods=['POST'])
+def create():
+    print(request.form)
+    User.save(request.form)
+    return redirect('/users')
+
+@app.route('/user/edit/<int:id>')
+def edit(id):
+    data ={
+        "id":id
+    }
+    return render_template("edit_user.html", user=User.get_one(data))
+
+@app.route('/user/display/<int:id>')
+def display(id):
+    data ={
+        "id":id
+    }
+    return render_template("display_user.html", user=User.get_one(data))
+
+@app.route('/user/update', methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/users')
+
+@app.route('/user/destroy/<int:id>')
+def destroy(id):
+    data ={
+        'id': id
+    }
+    User.destroy(data)
+    return redirect('/users')
+
+if __name__ == "__main__": 
+    app.run(debug=True)

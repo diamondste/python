ef4e7b820206fba31aff0f310257cab515858033
Diamond Stephens <diamond.stephens2@gmail.com>
2021-12-07 22:14:19 -0500

    commit -m "updated file and folders"

---
+++ flask/fundamentals/dojo_survey/flask_app/controllers/dojos.py
@@ -0,0 +1,19 @@
+from flask_app import app
+from flask import render_template, redirect, request
+from flask_app.models.dojo import Dojo
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/dojo/create', methods=['POST'])
+def create():
+    if Dojo.is_valid(request.form):
+        Dojo.save(request.form)
+        return redirect('/results')
+    return redirect('/')
+
+
+@app.route('/results')
+def result(id):
+    return render_template("result.html", dojo=Dojo.get_last())
--- flask/fundamentals/dojo_survey/server.py
+++ flask/fundamentals/dojo_survey/server.py
@@ -1,25 +1,5 @@
-from flask import Flask, render_template, request, redirect, session
-app = Flask(__name__)
-app.secret_key = 'shhhh...secret'
-# our index route will handle rendering our form
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route('/process', methods=['POST'])
-def submit():
-    print("Got Post Info")
-    # Never render a template on a POST request.
-    # Instead we will redirect to our index route.
-    session['username'] = request.form['name']
-    session['userlocation'] = request.form['location']
-    session['userlanguage'] = request.form['language']
-    session['usercomments'] = request.form['comments']
-    return redirect('/result')
-
-@app.route("/result")
-def show_user():
-    return render_template('result.html')
+from flask_app import app
+from flask_app.controllers import dojos
 
 if __name__ == "__main__":
     app.run(debug=True)

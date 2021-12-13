5f18741868102088684f3386a7401bc0180d8807
Diamond Stephens <diamond.stephens2@gmail.com>
2021-12-12 20:55:34 -0500

    commit -m "sign up and sign in"

---
+++ flask_mysql/validation/login_validation/flask_app/models/user.py
@@ -0,0 +1,67 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash 
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9.+_-]+\.[a-zA-Z0-9.+_-]+$')
+class User: 
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def save(cls, data):
+        query = "INSERT into user (first_name, last_name, email, password) VALUES(%(first_name)s , %(last_name)s , %(name)s , %(password)s);"
+        return connectToMySQL('register_login').mysql.query_db(query, data)
+    
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM user;"
+        results = connectToMySQL('register_login').query_db(query)
+        users = []
+        for user in results:
+            users.append(cls(user))
+        return users
+    
+    @classmethod
+    def get_one(cls, data):
+        query = "SELECT * FROM user WHERE id = %(id)s;"
+        result = connectToMySQL('register_login').query_db(query, data)
+        return cls(result[0])
+    
+    @classmethod
+    def get_by_email(cls, data):
+        query = "SELECT * FROM user WHERE email = %(email)s;"
+        result = connectToMySQL('register_login').query_db(query, data)
+
+        if len(result) < 1:
+            return False
+        return cls(result[0])
+
+    @staticmethod
+    def is_valid(user):
+        is_valid = True
+        query = "SELECT * FROM user WHERE email = %(email)s;"
+        results = connectToMySQL(User.db).query_db(query, user)
+        if len(results) >= 1:
+            flash("Email already taken.")
+            is_valid = False
+        if not EMAIL_REGEX.match(user['email']):
+            flash("Invaild Email")
+            is_valid = False 
+        if len(user['first_name']) < 2:
+            is_valid = False 
+            flash("Last  Name must be at least 2 characters.")
+        if len(user['last_name']) < 2:
+            is_valid = False 
+            flash("Last Name must be at least 2 characters.")
+        if len(user['password']) < 2:
+            is_valid = False 
+            flash("Password must be at least 8 characters.")
+        if user['password'] != user['confirm']: 
+            flash("Password doesn't match")
+        return is_valid
+        
